b906ba2c824123343276a9d3093e9101ba79dd2d
Areeff <p.areeff786@gmail.com>
2023-05-16 17:13:39 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
+    public static void main(String [] args){
+        Product p=new Product();
+        int ans1=p.product(3,5);
+        int ans2=p.product(3,4,5);
+        double ans3=p.product(2.3,3.4);
+    }
 }
